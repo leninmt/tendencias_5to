789b28fa5344b5c1ce2d8b6921b2629de29aca35
leninmt <ljc.montalvo@yavirac.edu.ec>
2024-05-06 08:54:43 -0500

    tt

--- src/main/java/Ejemplo/pructo/demo/categoria/CategoriaController.java
+++ src/main/java/Ejemplo/pructo/demo/categoria/CategoriaController.java
@@ -3,6 +3,7 @@ package Ejemplo.pructo.demo.categoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class CategoriaController {
 
     //select by id
     @GetMapping(value = "categoria/{id}")
-    public Categoria findById(Long id)
+    public Categoria findById(@PathVariable Long id)
     {
         return categoriaService.findById(id);
     }
